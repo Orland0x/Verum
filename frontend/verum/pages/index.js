40979c67918d05a4544b428c33b03274f11de145
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-07-07 15:11:29 +0100

    frontpage info

--- frontend/verum/pages/index.js
+++ frontend/verum/pages/index.js
@@ -8,13 +8,13 @@ export default function Home() {
       <div className="flex flex-col mt-14">
         <div className="flex flex-col md:flex-row justify-between">
           <div className="mx-5">
-            <h2 className="text-4xl text-green-700 font-bold">Truth based</h2>
+            <h2 className="text-4xl text-green-700 font-bold">Overview</h2>
             <p className="text-md mt-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt id elit sit amet porttitor. Nam accumsan gravida velit. Phasellus suscipit, nibh nec mollis tristique, sapien velit placerat felis, condimentum feugiat elit sem eget mauris. In ultricies metus et nulla varius, vitae consectetur ante tincidunt. Cras id volutpat arcu. Fusce magna nisl, tempor eget velit vitae, tincidunt volutpat ex. Vestibulum feugiat tellus sit amet varius tincidunt. Morbi nec tellus ex. Donec semper urna vitae finibus feugiat. Aenean sed dolor est. Morbi ante nisl, malesuada a enim quis, viverra varius ante. Nulla risus elit, ornare eu commodo in, elementum ac eros. In vel dapibus mi.
+            How can one trust what they see online? In an age of digital content, where anyone can post or share anything, it becomes increasingly difficult to have any trust in the content you are seeing. How can a blockchain solve this? The blockchain makes it very easy to track the origin of a piece of content that got posted and view the history of the person that posted it. We can then use this information to decide on how much trust to place in the content. 
             </p>
           </div>
           <div className="mx-5 mt-5 md:mt-0">
-            <img src="https://www.databentobox.com/2019/07/28/facebook-friend-graph/featured.png" width={2500} height={2500}></img>
+            <img src="https://www.womansera.com/wp-content/uploads/2019/10/Here%E2%80%99s-Why-Fake-News-Is-a-Threat-to-Your-Identity.jpg" width={2500} height={2500}></img>
           </div>
         </div>
         <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
@@ -22,9 +22,9 @@ export default function Home() {
             <img src="https://www.databentobox.com/2019/07/28/facebook-friend-graph/featured.png" width={2500} height={2500}></img>
           </div>
           <div className="mx-5">
-            <h2 className="text-4xl text-red-700 font-bold">Social Graph</h2>
+            <h2 className="text-4xl text-red-700 font-bold">How Verum Works</h2>
             <p className="text-md mt-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt id elit sit amet porttitor. Nam accumsan gravida velit. Phasellus suscipit, nibh nec mollis tristique, sapien velit placerat felis, condimentum feugiat elit sem eget mauris. In ultricies metus et nulla varius, vitae consectetur ante tincidunt. Cras id volutpat arcu. Fusce magna nisl, tempor eget velit vitae, tincidunt volutpat ex. Vestibulum feugiat tellus sit amet varius tincidunt. Morbi nec tellus ex. Donec semper urna vitae finibus feugiat. Aenean sed dolor est. Morbi ante nisl, malesuada a enim quis, viverra varius ante. Nulla risus elit, ornare eu commodo in, elementum ac eros. In vel dapibus mi.
+            There are 2 core actions that users can make: posting content to their own profile,  and attesting the truthfulness of other user profiles. Both of these actions are achieved via an on chain transaction that emits an event. Posts consist of the IPFS uri of the content while attestations consist of a value that encodes the users degree of trust in the profile. Now the real magic happens through indexing of these events using a subgraph. A user submits a list of addresses and the subgraph is queried for all the posts and attestations made by addresses in the list. It can then generate the full post history for each address along with a social graph that connects the addresses via their attestations of each other. In general, if a profile has a long post history and a high number of attestations over an extended period, then one can have high trust in the content posted. Verum makes it easy have trust in what you are seeing online.
             </p>
           </div>
         </div>
